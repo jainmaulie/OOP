5a2403f63b3dfff961c979f1874c2780d5390ac4
jainmaulie <jainmaulie@gmail.com>
2026-01-29 21:29:19 +0530

    Add files via upload

---
+++ Practical10.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Employee {
+
+    private String employeeName;
+    private double employeeSalary;
+
+    public void readEmployeeData() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter employee name:");
+        employeeName = sc.nextLine();
+
+        System.out.println("Enter employee salary:");
+        employeeSalary = sc.nextDouble();
+    }
+
+    public void displayEmployeeData() {
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Employee Salary: " + employeeSalary);
+    }
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee();
+
+        emp.readEmployeeData();
+        emp.displayEmployeeData();
+    }
+}
