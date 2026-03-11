a85ecd68161fcb9a064a9b7ed55207dde0843344
jainmaulie <jainmaulie@gmail.com>
2026-03-11 15:58:55 +0530

    Add files via upload

---
+++ Practical20.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Practical20 {
+
+    public static boolean isValidPassword(String password) {
+        int digitCount = 0;
+
+      
+        if (password.length() < 8) {
+            return false;
+        }
+
+  
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+          
+            if (!Character.isLetterOrDigit(ch)) {
+                return false;
+            }
+
+            // Count digits
+            if (Character.isDigit(ch)) {
+                digitCount++;
+            }
+        }
+
+      
+        if (digitCount < 2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a password: ");
+        String password = sc.nextLine();
+
+        if (isValidPassword(password)) {
+            System.out.println("Valid Password");
+        } else {
+            System.out.println("Invalid Password");
+        }
+
+        sc.close();
+    }
+}
