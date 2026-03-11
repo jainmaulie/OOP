9f02892c773506b8a2a1c2496654e8284f36ef92
jainmaulie <jainmaulie@gmail.com>
2026-03-11 16:01:46 +0530

    Add files via upload

---
+++ Practical21.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Practical21 {
+
+   
+    static double calculateVolume(double side) {
+        return side * side * side;
+    }
+
+   
+    static double calculateVolume(double length, double width, double height) {
+        return length * width * height;
+    }
+
+   
+    static double calculateVolume(int radius) {
+        return (4.0 / 3) * Math.PI * radius * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+     
+        System.out.print("Enter side of cube: ");
+        double side = sc.nextDouble();
+        System.out.println("Volume of Cube: " + calculateVolume(side));
+
+
+        System.out.print("Enter length, width, height of rectangular cube: ");
+        double l = sc.nextDouble();
+        double w = sc.nextDouble();
+        double h = sc.nextDouble();
+        System.out.println("Volume of Rectangular Cube: " + calculateVolume(l, w, h));
+
+     
+        System.out.print("Enter radius of sphere: ");
+        int r = sc.nextInt();
+        System.out.println("Volume of Sphere: " + calculateVolume(r));
+
+        sc.close();
+    }
+}
